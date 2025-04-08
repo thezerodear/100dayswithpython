1aa4bf7209dba33c1efe0d9e88e7aca3009502d3
takdanai.mp <110109253+thezerodear@users.noreply.github.com>
2025-04-08 14:04:44 +0700

    uploading days 1 project

---
+++ bandnamegenerator.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator!')
+city = input('What city did you grow up in?\n')
+pet = input('What is the name of a pet?\n')
+band_name = city + ' ' + pet
+print('Your band name could be ' + band_name + '!')
+
+# This program work by concatinate the two input form user and print out the result.
